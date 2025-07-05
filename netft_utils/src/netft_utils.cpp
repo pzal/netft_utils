53050822b18f9b8635e62ead061316c5e131b5fb
Piotr Zalewski <mail@pzalewski.com>
2025-07-05 16:47:56 +0200

    use right-hand rule for sensor axes

--- netft_utils/src/netft_utils.cpp
+++ netft_utils/src/netft_utils.cpp
@@ -283,10 +283,10 @@ void NetftUtils::netftCallback(const geometry_msgs::msg::WrenchStamped::ConstPtr
   // Filter data
   std::vector<double> tempData;
   tempData.resize(6);
-  tempData.at(0) = -data->wrench.force.x;
+  tempData.at(0) = data->wrench.force.x;
   tempData.at(1) = data->wrench.force.y;
   tempData.at(2) = data->wrench.force.z;
-  tempData.at(3) = -data->wrench.torque.x;
+  tempData.at(3) = data->wrench.torque.x;
   tempData.at(4) = data->wrench.torque.y;
   tempData.at(5) = data->wrench.torque.z;
 
